93fdea93a7ff9e6f9c830f50becc05dbbc556cf3
Megan Drace <Totoro@Megan-Draces-MacBook-Pro.local>
2012-11-25 22:10:00 -0800

    Added new test cases to deal with unicode fractions

--- test/app.js
+++ test/app.js
@@ -21,3 +21,50 @@ function getQuantity(line){
 	return [numbString, units, ingredient];
 
 }
+
+
+
+function getNumber(line) {
+	var ln = line.split(' ');
+	//extract the number from the beginning of the string and convert it to a number
+	var numb = parseInt(ln[0]);
+	if(isFraction(ln[0])) {
+		log("first");
+	}
+	if(isFraction(ln[1])) {
+		log("second");
+	}
+
+}
+
+function isFraction(element) {
+	var isVul = isUnicodeFraction(element);
+	var hasSlash = element.indexOf('/') >= 0;
+	var fracPieces = element.split('/');
+	hasNum = !isNaN(parseInt(fracPieces[0]))
+	hasDom = !isNaN(parseInt(fracPieces[1]))
+	return isVul || (hasSlash && hasNum && hasDom);
+}
+
+// check to see if it is a unicode vulgar fraction
+function isUnicodeFraction(char) {
+	var unicode = char.charCodeAt(0);
+	return ((unicode >= 188 && unicode <= 190) || (unicode >= 8531 && unicode <= 8542))
+}
+
+function indexOfCharFraction(line) {
+	var index = -1;
+	for (i=0; i<line.length; i++) {
+		if (isUnicodeFraction(line[i])) {
+			index = i;
+			break;
+		}
+	}
+	return index;
+}
+
+// check what the value of the vulgar fraction is
+
+
+//
+
--- test/test.js
+++ test/test.js
@@ -1,11 +1,3 @@
-console.log("-----");
-test( "hello test", function() {
-   ok( 1 == "1", "Passed!" );
-   strictEqual(1, "1", "oh my");
- });
-
-
-
 
 test("quantity test", function() {
  	deepEqual(getQuantity("1 cup flour"), ["1", "cup", "flour"], "simple list" );
@@ -18,6 +10,7 @@ test("quantity test", function() {
  	deepEqual(getQuantity("2 C pastry flour"), ["2", "C", "pastry flour"], "simple list" );
 });
 
+/*
 test("number conversions", function() {
 	equal(convertToNumber("3"), 3, "no conversions");
 	equal(convertToNumber("3 1/2"), 3.5, "fraction conversions");
@@ -25,4 +18,36 @@ test("number conversions", function() {
 	equal(convertToNumber("1 3/4"), 1.75, "fraction conversions");
 	equal(convertToNumber("1 &frac34"), 1.75, "ascii fraction conversions");
 })
+*/
 
+
+test("Is it a fraction", function () {
+	ok(!isFraction("3"), "whole numbers are not fractions");
+	ok(!isFraction("1 3"), "missing forward slash");
+	ok(isFraction("1/3"), "written out as common fraction");
+	ok(isFraction("1 / 3"), "space between seprator allowed");
+	ok(isFraction("1 /3"),  "space between seprator allowed");
+	ok(isFraction("1/ 3"),  "space between seprator allowed");
+	ok(isFraction("1/3"), "written out as common fraction");
+	ok(isFraction("999/999"), "common fraction with multiple chars on each side");
+	ok(isFraction('¼'), "fractional unicode chars are detected");
+	ok(isFraction('⅔'), "fractional unicode chars are detected");
+	ok(!isFraction("abc"), "alpha chars not allowed");
+	ok(!isFraction("'3 1/4'"), "fractions must be alone");
+	ok(!isFraction("1¼"), "starts with a whole number");
+})
+
+test ("Are unicode fractions recognized as fractions", function() {
+	var list = ['¼', '½', '¾', '⅓', '⅔', '⅕', '⅖', '⅗', '⅘', '⅙', '⅚', '⅛', '⅜', '⅝', '⅞'];
+	for (i=0; i < list.length; i++) {
+		ok(isUnicodeFraction(list[i]), "unicode characters are valid");
+	}
+	ok(!isUnicodeFraction('1/4'), "common fractions aren't vulgar");
+	ok(!isUnicodeFraction('1'), "whole numbers aren't vulgar fractions");
+	ok(!isUnicodeFraction('1¼'), "starts with a whole number");
+})
+/*
+test ("Is there a unicode fraction with a whole number", function() {
+
+})
+*/
