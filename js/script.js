5aa2534e6aa1e01073981b268fc8cfdb89582160
Megan Drace <dr.ace.m@gmail.com>
2013-03-08 22:08:48 -0800

    Made it awesome! Real-time editing of recipe and output created shadow div behind recipe input for text highlighting

--- js/script.js
+++ js/script.js
@@ -12,15 +12,37 @@ $(document).ready(function(){
 		};
 	});
 
-	// highlight the background of the textarea when it's in focus
-	$("#recipe_input").on({
-		blur : function(){
-			$(this).css("background-color", "#222b3b")
-		},
-		focus: function(){
-			$(this).css("background-color", "#1A212D")
-		}
+	function outputRecipe() {
+		var textBlock = $("#recipe_input").val();
+		var inputTextHtml = (textBlock).replace(/\n\r?/g, '<br />');
+		$("#backmodel").html(inputTextHtml);
+		var factor = $("#factor-list").val(); 
+		var allLines = multiplyRecipe(textBlock, factor);
+		var output = "";
+		for (var i = 0; i < allLines.length; i++) {
+			output = output + allLines[i]+"<br />";
+		};
+		$("#converted-recipe").html(output);
+	}
+
+
+	$("#recipe_input").bind('input propertychange', function(){
+		outputRecipe();
 	});
+
+	$("#factor-list").change(function(){
+		outputRecipe();
+	})
+
+	// highlight the background of the textarea when it's in focus
+	// $("#recipe_input").on({
+	// 	blur : function(){
+	// 		$(this).css("background-color", "#222b3b")
+	// 	},
+	// 	focus: function(){
+	// 		$(this).css("background-color", "#1A212D")
+	// 	}
+	// });
 
 	
 
