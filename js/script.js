7e77380b179b46a606f19fe5a10b95a068b67e93
Megan Drace <dr.ace.m@gmail.com>
2013-03-08 16:36:50 -0800

    highlight background of text area when selected added responsive layout

--- js/script.js
+++ js/script.js
@@ -8,9 +8,22 @@ $(document).ready(function(){
 	for (var i = 0; i < allLines.length; i++) {
 		allLines[i]
 		// $("#converted-recipe").append("<li>"+ allLines[i] +"</li>");
-		$("#converted-recipe").append(allLines[i]+"<br />");
+		$("#converted-recipe p").append(allLines[i]+"<br />");
 		};
 	});
+
+	// highlight the background of the textarea when it's in focus
+	$("#recipe_input").on({
+		blur : function(){
+			$(this).css("background-color", "#222b3b")
+		},
+		focus: function(){
+			$(this).css("background-color", "#1A212D")
+		}
+	});
+
+	
+
 
 	
 });
